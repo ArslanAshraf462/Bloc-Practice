68d66a1659b8b9f79106a06aaabdae7383891e2f
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2023-01-03 19:29:52 +0500

    feature/block-event-state: Added -> the bloc on ui with bloc consumer

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -1,3 +1,7 @@
 class AppStrings{
   static const String loadingText='Loading...';
+  static const String connectedText='Connected!';
+  static const String notConnectedText='Not Connected!';
+  static const String internetConnectedText='Internet Connected!';
+  static const String internetNotConnectedText='Internet Not Connected!';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_practice/bloc/internet_bloc/internet_bloc.dart';
 import 'package:bloc_practice/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
