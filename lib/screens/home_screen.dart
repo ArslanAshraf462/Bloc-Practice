b6ed783579c594dd0eecf13e00c31d4ebfd41c29
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2023-01-04 11:10:17 +0500

    feature/block-event-state: Added -> the custom snackbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_practice/bloc/internet_bloc/internet_bloc.dart';
 import 'package:bloc_practice/bloc/internet_bloc/internet_state.dart';
+import 'package:bloc_practice/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,27 +11,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Center(
-          child: BlocConsumer<InternetBloc,InternetState>(
-            listener: (context, state) {
-              if(state is InternetGainedState){
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text(AppStrings.internetConnectedText)));
-              }else if(state is InternetLostState){
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text(AppStrings.internetNotConnectedText)));
-              }
-            },
-            builder: (context,state) {
-                  if(state is InternetGainedState){
-                    return const Text(AppStrings.connectedText);
-                  } else if(state is InternetLostState){
-                    return const Text(AppStrings.notConnectedText);
-                  }else{
-                    return const Text(AppStrings.loadingText);
-                  }
+          child: BlocConsumer<InternetBloc, InternetState>(
+              listener: (context, state) {
+            if (state is InternetGainedState) {
+              showSnackBar(
+                context: context,
+                content: AppStrings.internetConnectedText,
+                color: AppColors.greenColor,
+              );
+            } else if (state is InternetLostState) {
+              showSnackBar(
+                context: context,
+                content: AppStrings.internetNotConnectedText,
+                color: AppColors.redColor,
+              );
             }
-          ),
+          }, builder: (context, state) {
+            if (state is InternetGainedState) {
+              return const Text(AppStrings.connectedText);
+            } else if (state is InternetLostState) {
+              return const Text(AppStrings.notConnectedText);
+            } else {
+              return const Text(AppStrings.loadingText);
+            }
+          }),
           // BlocBuilder<InternetBloc,InternetState>(
           //   builder: (context,state) {
           //     /// == means to check value
@@ -48,5 +55,16 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void showSnackBar(
+      {required BuildContext context,
+      required String content,
+      required Color color}) {
+    final snackBar = SnackBar(
+      content: Text(content),
+      backgroundColor: color,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
