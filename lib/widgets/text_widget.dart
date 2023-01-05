6c04f8acca1681b08770a34b8c3cfeef0b363414
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2023-01-05 17:35:06 +0500

    dev => Added the stream controller & stream builder example

--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -3,14 +3,26 @@ import 'package:flutter/material.dart';
 class TextWidget extends StatelessWidget {
   final String title;
   final Color? color;
-  const TextWidget({Key? key,required this.title,this.color}) : super(key: key);
+  final double? fontSize;
+  final FontWeight? fontWeight;
+
+  const TextWidget(
+      {Key? key,
+      required this.title,
+      this.color,
+      this.fontSize,
+      this.fontWeight})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(title,
-    style: TextStyle(
-      color: color,
-    ),
+    return Text(
+      title,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
     );
   }
 }
